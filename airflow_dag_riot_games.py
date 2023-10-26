6fa13e5f7b2d63759b2cbd0eae306b355ac25f4f
Alex Tran <alextran1357@gmail.com>
2023-10-26 14:02:26 -0700

    Add logging and extract all needed data from api json

--- airflow_dag_riot_games.py
+++ airflow_dag_riot_games.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-import json
+import logging
 import requests
 import os
 
@@ -39,6 +39,8 @@ def get_match_details(match_id, region, riot_api_key):
     match_details = response.json()
     return match_details
 
+
+
 def get_riot_data(**kwargs):
     summoner_name = "boosblues"
     region = "na1"
@@ -51,74 +53,102 @@ def get_riot_data(**kwargs):
     puuid = get_summoner_id(summoner_name, riot_api_key)
     match_ids = get_matchlist(puuid, riot_api_key)
     
+    # Set up logging
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    
     # need to handle some way of duplicate matches if i rerun the dag
     for match_id in match_ids:
         match_details = get_match_details(match_id, riot_api_key)
+        
+        # grab correct participant
+        try:
+            participants = match_details['metadata']['participants']
+            index = participants.index(puuid)
+        except ValueError:
+            logger.error("Participant ID not found in the list")
+            # Handle the error, e.g., by skipping to the next iteration, or setting index to None
+            index = None
+        except KeyError:
+            logger.error("The structure of the JSON is not as expected. 'metadata' or 'participants' key is missing.")
+            # Handle the error as appropriate
+            index = None
+            
         # Grab specific data
-        '''
-        check puuid
-        
-        GAME STATS -------------------------------------
-        gameCreation - long
-        win - boolean
-        gameEndedInSurrender - boolean
-        timePlayed - int
-        gameMode - string
-        lane - string
-        role - string
-        
-        CHAMPION STATS -------------------------------------
-        championName - string
-        champLevel - int
-        
-        KILL/ASSIST/DEATH STATS -------------------------------------
-        kills - int
-        assists - int
-        deaths - int
-        totalTimeSpentDead - int
-        doubleKills - int
-        tripleKills - int
-        quadraKills - int
-        pentaKills - int
-        totalMinionsKilled - int
-        totalAllyJungleMinionsKilled - int
-        dragonKills - int
-        baronKills - int
-        largestMultiKill - int
-        largestKillingSpree - int
-        
-        DAMAGE STATS -------------------------------------
-        physicalDamageDealt - int
-        physicalDamageDealtToChampions - int
-        physicalDamageTaken - int
-        magicDamageDealt - int	
-        magicDamageDealtToChampions - int	
-        magicDamageTaken - int	
-        trueDamageDealt - int
-        trueDamageDealtToChampions - int
-        trueDamageTaken - int
-        totalDamageDealt - int
-        totalDamageDealtToChampions - int
-        totalDamageTaken - int
-        totalHeal - int
-        totalHealsOnTeammates - int
-        totalTimeCCDealt - int
-        largestCriticalStrike - int
-        damageSelfMitigated - int
-        damageDealtToBuildings - int
-        damageDealtToObjectives - int
-        damageDealtToTurrets - int
-        
-        MISC STATS -------------------------------------
-        goldEarned - int
-        goldSpent - int
-        itemsPurchased - int
-        visionScore - int
-        visionWardsBoughtInGame - int
-        sightWardsBoughtInGame - int
-        wardsKilled - int
-        wardsPlaced - int
-        '''
+        # GAME STATS -------------------------------------
+        if index is not None:
+            # Grab specific data
+            try:
+                info = match_details.get("info", {})
+                gameCreation = info.get("gameCreation")
+                if gameCreation is None:
+                    logger.warning("The 'gameCreation' key is missing from 'info'.")
+                
+                participant_data = info.get("participants", [])[index] if info else None
+                if participant_data:
+                    # GAME STATS -------------------------------------
+                    win = participant_data.get('win')
+                    gameEndedInSurrender = participant_data.get('gameEndedInSurrender')
+                    timePlayed = participant_data.get('timePlayed')
+                    gameMode = participant_data.get('gameMode')
+                    lane = participant_data.get('lane')
+                    role = participant_data.get('role')
+                    
+                    # CHAMPION STATS -------------------------------------
+                    championName = participant_data.get('championName')
+                    champLevel = participant_data.get('champLevel')
+                    
+                    # KILL/ASSIST/DEATH STATS -------------------------------------
+                    kills = participant_data.get('kills')
+                    assists = participant_data.get('assists')
+                    deaths = participant_data.get('deaths')
+                    totalTimeSpentDead = participant_data.get('totalTimeSpentDead')
+                    doubleKills = participant_data.get('doubleKills')
+                    tripleKills = participant_data.get('tripleKills')
+                    quadraKills = participant_data.get('quadraKills')
+                    pentaKills = participant_data.get('pentaKills')
+                    totalMinionsKilled = participant_data.get('totalMinionsKilled')
+                    totalAllyJungleMinionsKilled = participant_data.get('totalAllyJungleMinionsKilled')
+                    dragonKills = participant_data.get('dragonKills')
+                    baronKills = participant_data.get('baronKills')
+                    largestMultiKill = participant_data.get('largestMultiKill')
+                    largestKillingSpree = participant_data.get('largestKillingSpree')
+                    
+                    # DAMAGE STATS -------------------------------------
+                    physicalDamageDealt = participant_data.get('physicalDamageDealt')
+                    physicalDamageDealtToChampions = participant_data.get('physicalDamageDealtToChampions')
+                    physicalDamageTaken = participant_data.get('physicalDamageTaken')
+                    magicDamageDealt = participant_data.get('magicDamageDealt')
+                    magicDamageDealtToChampions = participant_data.get('magicDamageDealtToChampions')
+                    magicDamageTaken = participant_data.get('magicDamageTaken')
+                    trueDamageDealt = participant_data.get('trueDamageDealt')
+                    trueDamageDealtToChampions = participant_data.get('trueDamageDealtToChampions')
+                    trueDamageTaken = participant_data.get('trueDamageTaken')
+                    totalDamageDealt = participant_data.get('totalDamageDealt')
+                    totalDamageDealtToChampions = participant_data.get('totalDamageDealtToChampions')
+                    totalDamageTaken = participant_data.get('totalDamageTaken')
+                    totalHeal = participant_data.get('totalHeal')
+                    totalHealsOnTeammates = participant_data.get('totalHealsOnTeammates')
+                    totalTimeCCDealt = participant_data.get('totalTimeCCDealt')
+                    largestCriticalStrike = participant_data.get('largestCriticalStrike')
+                    damageSelfMitigated = participant_data.get('damageSelfMitigated')
+                    damageDealtToBuildings = participant_data.get('damageDealtToBuildings')
+                    damageDealtToObjectives = participant_data.get('damageDealtToObjectives')
+                    damageDealtToTurrets = participant_data.get('damageDealtToTurrets')
+                    
+                    # MISC STATS ------------------------------------- 8
+                    goldEarned = participant_data.get('goldEarned')
+                    goldSpent = participant_data.get('goldSpent')
+                    itemsPurchased = participant_data.get('itemsPurchased')
+                    visionScore = participant_data.get('visionScore')
+                    visionWardsBoughtInGame = participant_data.get('visionWardsBoughtInGame')
+                    sightWardsBoughtInGame = participant_data.get('sightWardsBoughtInGame')
+                    wardsKilled = participant_data.get('wardsKilled')
+                    wardsPlaced = participant_data.get('wardsPlaced')
+                else:
+                    logger.error("Participants data not found or index out of range.")
+            except Exception as e:
+                logger.error(f"An unexpected error occurred: {str(e)}")
 
 def process_riot_data(**kwargs):
     task_instance = kwargs['ti']
